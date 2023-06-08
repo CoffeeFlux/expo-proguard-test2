9fd4d81d83fcf983b96f04003dad373f73e5857d
Ryan Lucia <ryan@luciaonline.net>
2023-06-08 13:41:34 -0700

    Changes

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const onPress = React.useCallback(async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
+      selectionLimit: 16,
+      orderedSelection: true,
+      quality: 1,
+    });
+    console.error(result);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Press me" onPress={onPress} />
       <StatusBar style="auto" />
     </View>
   );
